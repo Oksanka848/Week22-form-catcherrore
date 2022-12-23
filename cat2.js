524c1bd4dd95febd91e7cfa6ec87067ffbe56d04
Oksanaka848 <okmitka@mail.ru>
2022-12-23 22:26:13 +0300

    ch

--- cat2.js
+++ cat2.js
@@ -3,7 +3,7 @@ button.addEventListener(
   "click",
   function () {
     check()
-    
+    myFunction()
   },
 
   class Cat {
@@ -26,6 +26,25 @@ button.addEventListener(
     mycat = console.log(mycat);
   }
 );
+
+function myFunction() {
+  let message, x;
+  message = document.getElementById("error_name");
+  message.innerHTML = "";
+  x = document.getElementById("name").value;
+  try {
+      if(x == "") throw "пусто";
+      
+      if(x.length < 2) throw "слишком коротко";
+      if(x.length > 15) throw "слишком много буков)";
+  }
+  catch(err) {
+      message.innerHTML += "Ошибка: " + err + ".";
+  }
+  finally {
+      document.getElementById("name").value;
+  }
+}
 
 function check() {
   document.getElementById("error").innerHTML = " ";
@@ -53,6 +72,31 @@ function sentIt() {
       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
     },
   })
+  .then((response) => {
+    if (response.status >= 200 && response.status < 300) {
+        return response;
+    } else {
+        let error = new Error(response.statusText);
+        error.response = response;
+        throw error
+    }
+})
+/*.then((response) => {
+  if (response.headers['method'] !== 'DELETE') {
+      let error = new Error('Не тот метод');
+      error.response = response;
+      throw error
+  }
+  return response;
+})
+.then((response) => {
+    if (response.headers['content-type'] !== 'application/json') {
+        let error = new Error('Некорректный ответ от сервера');
+        error.response = response;
+        throw error
+    }
+    return response;
+})*/
     .then((response) => response.json())
     .then((mycat) => {
       console.log(mycat);
